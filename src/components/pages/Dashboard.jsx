3198bcdac260db0062ad6f3ab19c527360cfba97
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2020-12-03 13:20:30 +0800

    dashboard updated. able to show the join status on the page

--- src/components/pages/Dashboard.jsx
+++ src/components/pages/Dashboard.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import axios from "axios";
 import qs from "qs";
 import { Link } from "react-router-dom";
+import { withCookies } from "react-cookie";
+import "./Dashboard.scss";
 
 class Dashboard extends React.Component {
   constructor(props) {
@@ -12,6 +14,41 @@ class Dashboard extends React.Component {
     };
   }
 
+  componentDidMount() {
+    this.getLocationNearUser();
+  }
+
+  getLocationNearUser() {
+    axios
+      .get("http://localhost:5000/api/v1/orders-location", {
+        headers: {
+          auth_token: this.props.cookies.get("token"),
+        },
+      })
+      .then((response) => {
+        this.setState({
+          location: response.data,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  userFound(users) {
+    let found = users.find((eachUser) => {
+      return eachUser === this.getCurrentUser().user_id;
+    });
+    return found;
+    // let found = false;
+    // users.find((eachUser) => {
+    //   if (eachUser === this.getCurrentUser().user_id) {
+    //     return (found = true);
+    //   }
+    // });
+    // return found;
+  }
+
   getCurrentUser() {
     return JSON.parse(localStorage.getItem("user"));
   }
@@ -20,10 +57,15 @@ class Dashboard extends React.Component {
     // e.preventDefault();
     axios
       .post(
-        "http://localhost:5000/api/v1/location",
+        "http://localhost:5000/api/v1/orders-location",
         qs.stringify({
           address: this.state.address,
-        })
+        }),
+        {
+          headers: {
+            auth_token: this.props.cookies.get("token"),
+          },
+        }
       )
       .then((response) => {
         this.setState({
@@ -46,86 +88,105 @@ class Dashboard extends React.Component {
   render() {
     return (
       <div className="page-dashboard">
-        <p>
-          Welcome{" "}
-          {this.getCurrentUser().first_name +
-            " " +
-            this.getCurrentUser().last_name}
-        </p>
-        <form className="form-inline my-2 my-lg-0 home-page-form">
-          <input
-            className="form-control location-input"
-            type="text"
-            placeholder="Enter your location"
-            name="address"
-            onChange={(e) => {
-              this.handleChange(e);
-            }}
-          />
-          <div
-            onClick={(e) => {
-              this.handleSubmit(e);
-            }}
-            className="btn btn-outline-success my-2 my-sm-0 search-btn"
-          >
-            Search
-          </div>
-        </form>
-        {this.state.location !== "" ? (
-          <div className="location-near-you">
-            <div className="container">
-              {this.state.location.length > 0 ? <h1>Someone Near You</h1> : ""}
-              <div className="row">
+        <div className="container">
+          <p className="welcome-message">
+            Welcome{" "}
+            {this.getCurrentUser().first_name +
+              " " +
+              this.getCurrentUser().last_name}
+          </p>
+          {/* <button class="btn btn-dark" type="submit">
+            <i className="fa fa-search"></i>
+          </button> */}
+          <form className="form-inline my-2 my-lg-0 home-page-form">
+            <input
+              className="form-control location-input"
+              type="text"
+              placeholder="Enter your location"
+              name="address"
+              onChange={(e) => {
+                this.handleChange(e);
+              }}
+            />
+            <div
+              onClick={(e) => {
+                this.handleSubmit(e);
+              }}
+              className="btn btn-outline-success my-2 my-sm-0 search-btn"
+            >
+              Search
+            </div>
+          </form>
+          {this.state.location !== "" ? (
+            <div className="location-near-you">
+              <div className="container">
                 {this.state.location.length > 0 ? (
-                  this.state.location.map((item) => {
-                    return (
-                      <div className="col-4" key={item._id}>
-                        <div className="restaurant">{item.restaurant}</div>
-                        <div>
-                          Total: <strong>${item.deliveryFee}</strong>
-                          <span className="delivery-fee"> delivery fee</span>
-                        </div>
-                        <div>
-                          No. of pax:
-                          <strong> {item.usersjoined.length}</strong>
-                        </div>
-                        <span className="dis">
-                          <i className="fas fa-people-arrows"></i>
-                          <span> {Math.floor(item.dis)} m </span>
-                        </span>
-                        <span className="time">
-                          <i className="fas fa-clock"></i>
-                          <span> {item.deliveryTimeEst} min</span>
-                        </span>
-                        <Link
-                          to={{
-                            pathname: `/orders/${item._id}`,
-                          }}
-                        >
-                          <div className="lets-join-btn">
-                            <button className="btn btn-primary">
-                              <i className="fas fa-info-circle">
-                                {" "}
-                                See Order Details
-                              </i>
-                            </button>
-                          </div>
-                        </Link>
-                      </div>
-                    );
-                  })
+                  <h1>Someone Near You</h1>
                 ) : (
-                  <div>Sorry. No one is ordering near you</div>
+                  ""
                 )}
+                <div className="row">
+                  {this.state.location.length > 0 ? (
+                    this.state.location.map((item) => {
+                      return (
+                        <div className="col-4" key={item._id}>
+                          {this.userFound(item.usersjoined) ? (
+                            <div> joined</div>
+                          ) : (
+                            <div>not joined</div>
+                          )}
+
+                          <div className="restaurant">{item.restaurant}</div>
+                          <div>Created by: {item.userid}</div>
+                          <div>
+                            Total: <strong>${item.deliveryFee}</strong>
+                            <span className="delivery-fee"> delivery fee</span>
+                          </div>
+                          <div>
+                            No. of pax:
+                            <strong>
+                              {" "}
+                              {item.usersjoined.length + 1} (currently)
+                            </strong>
+                          </div>
+                          <span className="dis">
+                            <i className="fas fa-people-arrows"></i>
+                            <span> {Math.floor(item.dis)} m </span>
+                          </span>
+                          <span className="time">
+                            <i className="fas fa-clock"></i>
+                            <span> {item.deliveryTimeEst} min</span>
+                          </span>
+                          <Link
+                            to={{
+                              pathname: `/orders/${item._id}`,
+                            }}
+                          >
+                            <div className="lets-join-btn">
+                              <button className="btn btn-primary">
+                                <i className="fas fa-info-circle">
+                                  {" "}
+                                  See Order Details
+                                </i>
+                              </button>
+                            </div>
+                          </Link>
+                        </div>
+                      );
+                    })
+                  ) : (
+                    <div>Sorry. No one is ordering near you</div>
+                  )}
+                </div>
               </div>
             </div>
-          </div>
-        ) : (
-          ""
-        )}
+          ) : (
+            ""
+          )}
+        </div>
       </div>
     );
   }
 }
 
-export default Dashboard;
+export default withCookies(Dashboard);
